d08dc9511f8b39e9fef2da1261342d365f97cc59
borney <borneywpf@gmail.com>
2017-08-29 20:33:38 +0800

    modify for update-filelist.sh

--- vimfiles/tools/shell/bash/update-filelist.sh
+++ vimfiles/tools/shell/bash/update-filelist.sh
@@ -16,34 +16,39 @@ fi
 # get filelist
 echo "  |- generate ${TMP}"
 
-echo FOLDERS=${FOLDERS}
-echo FORCE_POSIX_REGEX_1=${FORCE_POSIX_REGEX_1}
-echo FORCE_POSIX_REGEX_2=${FORCE_POSIX_REGEX_2}
-echo IS_EXCLUDE=${IS_EXCLUDE}
-echo FILE_SUFFIXS=${FILE_SUFFIXS}
-echo TMP=${TMP}
-echo TARGET=${TARGET}
-echo DATA_TMP=${DATA_TMP}
-echo DATA_TARGET=${DATA_TARGET}
+#echo FOLDERS=${FOLDERS}
+#echo FORCE_POSIX_REGEX_1=${FORCE_POSIX_REGEX_1}
+#echo FORCE_POSIX_REGEX_2=${FORCE_POSIX_REGEX_2}
+#echo IS_EXCLUDE=${IS_EXCLUDE}
+#echo FILE_SUFFIXS=${FILE_SUFFIXS}
+#echo TMP=${TMP}
+#echo TARGET=${TARGET}
+#echo DATA_TMP=${DATA_TMP}
+#echo DATA_TARGET=${DATA_TARGET}
+
+ROOT_DIR=`pwd`
 
 if test "${FOLDERS}" != ""; then
-    find ${FORCE_POSIX_REGEX_1} ./ -type f -not -path "*/\.*" ${FORCE_POSIX_REGEX_2} ${IS_EXCLUDE} -regex ".*/("${FOLDERS}")/.*" ${FORCE_POSIX_REGEX_2} -regex ".*\.("${FILE_SUFFIXS}")$" > "${TMP}"
-
-    if [[ "${FILE_SUFFIXS}" != __EMPTY__ ]]; then
-        find ${FORCE_POSIX_REGEX_1} ./ -type f -not -path "*/\.*" ${FORCE_POSIX_REGEX_2} ${IS_EXCLUDE} -regex ".*/("${FOLDERS}")/.*" ${FORCE_POSIX_REGEX_2}  |grep  -v  "\.\w*$" |xargs -i sh -c 'file="{}";type=$(file $file);[[ $type =~ "text" ]] && echo $file' >> "${TMP}"
+    find ${FORCE_POSIX_REGEX_1} ${ROOT_DIR} -type f -not -path "*/\.*" ${FORCE_POSIX_REGEX_2} ${IS_EXCLUDE} -regex ".*/("${FOLDERS}")/.*" ${FORCE_POSIX_REGEX_2} -regex ".*\.("${FILE_SUFFIXS}")$" > "${TMP}"
+    if [ "${FILE_SUFFIXS}" != __EMPTY__  ]; then
+        find ${FORCE_POSIX_REGEX_1} ${ROOT_DIR} -type f -not -path "*/\.*" ${FORCE_POSIX_REGEX_2} ${IS_EXCLUDE} -regex ".*/("${FOLDERS}")/.*" ${FORCE_POSIX_REGEX_2}  | grep  -v  "\.\w*$" |xargs -i sh -c 'file="{}";type=$(file $file);[ "$type" != "text" ] && echo $file' >> "${TMP}"
     fi
 
 else
-    find ${FORCE_POSIX_REGEX_1} ./ -type f -not -path "*/\.*" ${FORCE_POSIX_REGEX_2} -regex ".*\.("${FILE_SUFFIXS}")$" > "${TMP}"
+    find ${FORCE_POSIX_REGEX_1} ${ROOT_DIR} -type f -not -path "*/\.*" ${FORCE_POSIX_REGEX_2} -regex ".*\.("${FILE_SUFFIXS}")$" > "${TMP}"
 
     if [ "${FILE_SUFFIXS}" != __EMPTY__  ]; then
-        find ${FORCE_POSIX_REGEX_1} ./ -type f -not -path "*/\.*" ${FORCE_POSIX_REGEX_2} -regex ".*\.("${FILE_SUFFIXS}")$" |grep  -v  "\.\w*$" |xargs -i sh -c 'file="{}";type=$(file $file);[ "$type" != "text" ] && echo $file' >> "${TMP}"
+        find ${FORCE_POSIX_REGEX_1} ${ROOT_DIR} -type f -not -path "*/\.*" ${FORCE_POSIX_REGEX_2} -regex ".*\.("${FILE_SUFFIXS}")$" |grep  -v  "\.\w*$" |xargs -i sh -c 'file="{}";type=$(file "$type");[ "$type" != "text" ] && echo $file' >> "${TMP}"
     fi
 
 fi
-
+set -x
 echo "  |- generate ${DATA_TMP}"
-     find ${FORCE_POSIX_REGEX_1} ./  -type f -not -path "./.*" -name '*.c' -o -name '*.cpp' -o -name '*.java' -o -name '*.h' -o -name "*.cc" -o -name "*.l" -o -name "*.y" | egrep -v "\/out\/" | sort -f >${DATA_TMP}
+if test "${FOLDERS}" != ""; then
+     find ${FORCE_POSIX_REGEX_1} ${ROOT_DIR} -type f -not -path "*/\.*" ${FORCE_POSIX_REGEX_2} ${IS_EXCLUDE} -regex ".*/("${FOLDERS}")/.*" ${FORCE_POSIX_REGEX_2} -regex ".*\.("${FILE_SUFFIXS}")$" > "${DATA_TMP}"
+else
+   find ${FORCE_POSIX_REGEX_1} ${ROOT_DIR} -type f -not -path "*/\.*" ${FORCE_POSIX_REGEX_2} -regex ".*\.("${FILE_SUFFIXS}")$" > "${DATA_TMP}"
+fi
 
 # DISABLE
 # # find . -type f -not -path "*/\.*" > "${TMP}"
