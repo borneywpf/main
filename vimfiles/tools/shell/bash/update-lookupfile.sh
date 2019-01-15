791379484be2884b41a0b0ec5f01b89fd8a4ead2
borney <borneywpf@gmail.com>
2019-01-15 15:04:50 +0800

    修改update-lookupfile.sh兼容mac

--- vimfiles/tools/shell/bash/update-lookupfile.sh
+++ vimfiles/tools/shell/bash/update-lookupfile.sh
@@ -3,29 +3,27 @@
 # create lookupfile
 echo "Creating Lookupfile..."
 
-ROOT_DIR=`pwd`
-
-# test posix regex
-find $ROOT_DIR -maxdepth 1 -regextype posix-extended -regex "test" > /dev/null 2>&1
-if test "$?" = "0"; then
-    FORCE_POSIX_REGEX_1=""
-    FORCE_POSIX_REGEX_2="-regextype posix-extended"
-else
-    FORCE_POSIX_REGEX_1="-E"
-    FORCE_POSIX_REGEX_2=""
-fi
+TMP_FILE=${TMP}.tmp
 
 echo "  |- generate ${TARGET}"
 
-TMP_FILE=${TMP}.tmp
-
+# choose ctags path first
 echo -e "!_TAG_FILE_SORTED\t2\t/2=foldcase/" > ${TMP_FILE}
-if test "${FOLDERS}" != ""; then
-    find ${FORCE_POSIX_REGEX_1} . -type f -not -path "*/\.*" ${FORCE_POSIX_REGEX_2} ${IS_EXCLUDE} -regex ".*/("${FOLDERS}")/.*" ${FORCE_POSIX_REGEX_2} -regex ".*\.("${FILE_SUFFIXS}")$" -exec grep -Iq . {} \; -and -printf "%f\t%p\t1\n" >> "${TMP_FILE}"
+if [ -f "${DEST}/data.files" ]; then
+    FILES="${DEST}/data.files"
+    cat ${FILES} | while read line
+    do
+        name=`basename $line`
+        echo "${name}\t${line}\t1" >> "${TMP_FILE}"
+    done
 else
-    find ${FORCE_POSIX_REGEX_1} . -type f -not -path "*/\.*" ${FORCE_POSIX_REGEX_2} -regex ".*\.("${FILE_SUFFIXS}")$" -exec grep -Iq . {} \; -and  -printf "%f\t%p\t1\n" >> "${TMP_FILE}"
+    if test "${FOLDERS}" != ""; then
+        find ${FORCE_POSIX_REGEX_1} . -type f -not -path "*/\.*" ${FORCE_POSIX_REGEX_2} ${IS_EXCLUDE} -regex ".*/("${FOLDERS}")/.*" ${FORCE_POSIX_REGEX_2} -regex ".*\.("${FILE_SUFFIXS}")$" -exec grep -Iq . {} \; -and -printf "%f\t%p\t1\n" >> "${TMP_FILE}"
+    else
+        find ${FORCE_POSIX_REGEX_1} . -type f -not -path "*/\.*" ${FORCE_POSIX_REGEX_2} -regex ".*\.("${FILE_SUFFIXS}")$" -exec grep -Iq . {} \; -and  -printf "%f\t%p\t1\n" >> "${TMP_FILE}"
+    fi
+    #find ${ROOT_DIR} -not -regex '.*\.\(png\|gif\)' -type f ! -path "./.*" ! -path "${ROOT_DIR}/out/*" -printf "%f\t%p\t1\n" >> ${TMP}
 fi
-#find ${ROOT_DIR} -not -regex '.*\.\(png\|gif\)' -type f ! -path "./.*" ! -path "${ROOT_DIR}/out/*" -printf "%f\t%p\t1\n" >> ${TMP}
 
 if [ -f "${TMP_FILE}" ]; then
     echo "  |- move ${TMP_FILE} to ${TARGET}"
